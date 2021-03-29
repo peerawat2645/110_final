23b49a8f2228409e28ba57b5b6c6988922fe8979
Peerawat Wongmek <peerawat.won@ku.th>
2021-03-29 19:40:23 +0700

    add

--- looblike.cpp
+++ looblike.cpp
@@ -1,28 +1,28 @@
 #include <stdio.h>
 
 int main(){
-	int n,i,k,max=0,j=0;
+	int n,i,k,max=0,min,j=0,a=0;
 	scanf("%d",&n);
 	if(n>=2&&n<=1000){
 		int l[1001]={0};
 		int t[n];
 		for(i=0;i<n;i++){
 			scanf("%d",&k);
-			if(k>=1&&k<=10000)
-			l[k]++;
-			
+			if(k>=1&&k<=10000){
+				l[k]++;
+			}
 		}
 		for(i=1;i<=1000;i++){
-			if(max<=l[i]){
-				if(l[i]>=0){
-					t[j]=i;
-					j++;
-				}
+			if(max<l[i]){	
 				max=l[i];
-				
 			}
-		
 		}
+		for(i=1;i<=1000;i++)
+			if(l[i]==max)
+			{
+				t[j]=i;
+				j++;
+			}
 		for(i=0;i<j;i++)
 			printf("%d ",t[i]);
 	}
