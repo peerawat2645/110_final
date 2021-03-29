2a54324fe121fbd6893f2d5a51d3ce22e5582fcf
Peerawat Wongmek <peerawat.won@ku.th>
2021-03-29 17:30:28 +0700

    add

---
+++ looblike.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+	int n,i,k,max=0,j=0;
+	scanf("%d",&n);
+	if(n>=2&&n<=1000){
+		
+	}
+	int l[1001]={0};
+	int t[n];
+	for(i=0;i<n;i++){
+		scanf("%d",&k);
+		l[k]++;
+	}
+	for(i=1;i<=1000;i++){
+		if(max<l[i]){
+			t[j]=i;
+			max=i;
+			j++;
+		}
+		
+	}
+	for(i=0;i<j;i++)
+		printf("%d ",t[i]);
+	return 0;
+}
